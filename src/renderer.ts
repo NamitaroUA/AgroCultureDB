4825b32d0fd1488e2900b17139ad3f42029ebffc
NamitaroUA <bohdanwork@proton.me>
2026-09-14 18:54:21 +0300

    Initial setup

---
+++ src/renderer.ts
@@ -0,0 +1,5 @@
+const information = document.getElementById('info')
+
+if (information) {
+    information.innerText = `This app is using Chrome (v${window.versions.chrome()}), Node.js (v${window.versions.node()}), and Electron (v${window.versions.electron()})`
+}
